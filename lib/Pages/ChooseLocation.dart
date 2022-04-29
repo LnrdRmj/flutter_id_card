bcc8b1330c49570dcf074718034f02ff0d0d2ed3
Stratos Spa <leonardo.ra14@icloud.com>
2022-04-29 17:58:11 +0200

    Chooes your location
    Finished tutorial

--- lib/Pages/ChooseLocation.dart
+++ lib/Pages/ChooseLocation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../services/world_time.dart';
+
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key? key}) : super(key: key);
 
@@ -9,6 +11,32 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+  void updateTime(int index) async {
+
+    WorldTime worldTime = locations[index];
+
+    await worldTime.getTime();
+
+    Navigator.pop(context, {
+      'time': worldTime.time,
+      'location': worldTime.location,
+      'flag': worldTime.flag
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +47,23 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: SafeArea(
-          child: Text('Choose Location')
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+
+          return Card(
+            child: ListTile(
+              title: Text(locations[index].location),
+              onTap: () {
+                updateTime(index);
+              },
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/${locations[index].flag}'),
+              ),
+            ),
+          );
+
+        },
       ),
     );
   }
--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    data = ModalRoute.of(context)!.settings.arguments as Map;
+    data = data.isNotEmpty ? data : ModalRoute.of(context)!.settings.arguments as Map;
 
     return Scaffold(
       body: SafeArea(
@@ -27,8 +27,16 @@ class _HomeState extends State<Home> {
                 Padding(
                   padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                   child: OutlinedButton.icon(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/location');
+                      onPressed: () async {
+                        dynamic result = await Navigator.pushNamed(context, '/location') as Map;
+                        setState(() {
+                          data = {
+                            'time': result['time'],
+                            'location': result['location'],
+                            'isDaytime': result['isDaytime'],
+                            'flag': result['flag']
+                          };
+                        });
                       },
                       icon: Icon(Icons.location_on_outlined),
                       label: Text('Edit location'),
